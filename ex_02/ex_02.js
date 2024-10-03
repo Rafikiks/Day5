b3582db045c34f7fc129c934816d6a352799435a
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-03 21:13:54 +0200

    Create ex_02.js

---
+++ ex_02/ex_02.js
@@ -0,0 +1,9 @@
+const paragraphs = document.querySelectorAll('#description p');
+    paragraphs[0].innerHTML = '0';
+    paragraphs[1].innerHTML = '1';
+    paragraphs[2].innerHTML = '2';
+
+
+
+// Sélectionne tous les paragraphes à l'intérieur de l'élément avec l'ID "description" 
+// et modifie leur contenu pour afficher leur index respectif.
