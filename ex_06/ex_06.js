b70ad74faadd0fb3c91a193f4875264bc42d8aa3
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-03 21:23:43 +0200

    Create ex_06.js

---
+++ ex_06/ex_06.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', function() {
+    alert('Size matters');
+});
+const phoneImage = document.getElementById('phoneImage');
+phoneImage.addEventListener('mouseover', function() {
+    alert("Pretty, isn't it?");
+});
+
+
+// Affiche un message lorsque la fenêtre est redimensionnée.
