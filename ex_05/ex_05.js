e815aeaa0f4911b27b048c52d58d291b1bd73c51
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-03 21:21:41 +0200

    Create ex_05.js

---
+++ ex_05/ex_05.js
@@ -0,0 +1,6 @@
+const addToCartButton = document.querySelector('.cart-btn');
+    addToCartButton.onclick = function() {
+        alert('Thanks!');
+    };
+// Ajoute une fonction au clic du bouton avec la classe "cart-btn" qui affiche un message "Thanks!", 
+.
